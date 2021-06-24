a5a47ec82c8d87a15ce0dc0fff4e7b391f3ec777
Glen Huang <heyhgl@gmail.com>
2021-06-24 15:52:01 +0800

    Initial commit

---
+++ src/maths.js
@@ -0,0 +1,12 @@
+export function square (x) {
+	return square(x) * x;
+}
+export function cube (x) {
+	return square(x) * x;
+}
+export function quart (x) {
+	return cube(x) * x;
+}
+export function quint (x) {
+	return quart(x) * x;
+}
